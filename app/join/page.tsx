4c32691313c6b1a37564317e3206ddc18815019d
cxuri <rootrcz@gmail.com>
2025-12-30 21:48:36 +0530

    test

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -4,8 +4,18 @@ import { loadCookie } from '../actions';
 import JoinContent from './JoinContent';
 
 export default async function Page() {
-  const ticketId = await loadCookie(); 
 
+  const rawData = await loadCookie();  
+  let ticketId = null;
+
+  if (rawData) {
+    try {
+      const parsedData = JSON.parse(rawData);
+      ticketId = parsedData.ticketId;
+    } catch (e) {
+      ticketId = rawData;
+    }
+  }
   return (
     <JoinContent initialTicketId={ticketId} />
   );
